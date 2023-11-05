375baa3441fd1e748018fe91c6c34aabba9a1fc7
Artem Markelov <markelovartemka@icloud.com>
2023-11-05 15:10:34 -0700

    Cleaned code -> next part - change whole layout of webpage, single page and route through it

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../style.css'
 
-function Main() {
+function Main(props) {
 
     const [hamburgerOpen, setHamburgerOpen] = React.useState(false)
 
