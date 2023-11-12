b00f7ee5f42937ee16f496d73b68b0cc35c9e203
Artem Markelov <markelovartemka@icloud.com>
2023-11-12 13:45:16 -0700

    Added links to main buttons

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from './Layout'
+import {Link} from 'react-router-dom'
 import '../style.css'
 
 function Main(props) {
@@ -11,8 +12,12 @@ function Main(props) {
                 <h2 className = 'title main'>Welcome!</h2>
                 <h1 className = 'subtitle main'>I am a Web Developer from Colorado Springs</h1>
                 <div className = 'main-btns'>
-                    <button className = 'main-btn portfolio'>Portfolio</button>
-                    <button className = 'main-btn about'>About</button>
+                    <button className = 'main-btn portfolio'>
+                        <Link to ='/cover'>Portfolio</Link>
+                    </button>
+                    <button className = 'main-btn about'>
+                        <Link to = '/about'>About</Link>
+                    </button>
                 </div>
             </div>
         </div>
